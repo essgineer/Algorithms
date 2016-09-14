e228560a581573a765a44d2bf87d6125f3234959
Essgineer <b.essqali@gmail.com>
2016-09-14 14:13:34 +0100

    Remark
    My score: 84%
    to have 100% use Math.max(). ;)

---
+++ MaxSliceSum.java
@@ -0,0 +1,17 @@
+public class Solution {
+	int sum=0, i=0,j=0;
+	public int solution(int[] A){
+		int max=A[0];
+		while(j<A.length){
+			sum+=A[i];
+			max= (sum>max)? sum:max;
+			i++;  
+			if(i==A.length){
+				j++;
+			  i=j;
+			  sum=0;
+			}
+		}	
+		return max;
+	}
+}
